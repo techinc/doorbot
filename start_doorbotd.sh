c27e47ce80f7000b69e5c10d453e8134a5e42200
Erik Bosman <erik@minemu.org>
2014-02-14 14:05:58 +0000

    simplefy startin program, make it work with init

--- start_doorbotd.sh
+++ start_doorbotd.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-(
-	cd "$(dirname "$0")"
-	su "$(stat -c "%U" user.db)" -c "while true; do python ./doorbotd.py; sleep 5; done"
-)&
+ERRLOG=/srv/doorbot/doorbot.err
+cd "$(dirname "$0")"
+su "$(stat -c "%U" user.db)" -c "while true; do python ./doorbotd.py; sleep 5; done" >>"$ERRLOG" 2>&1 &
